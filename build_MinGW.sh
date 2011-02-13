7aaeda56c7cedc376bfd3e7e8061c73fb5ad4f1e
Rafal Lalik <RafalLalik@ph.tum.de>
2011-02-13 01:03:19 +0100

    build scripts for Linux and MinGW

---
+++ build_MinGW.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set zarlok_src=`pwd`
+mkdir %zarlok_src%/build
+cd %zarlok_src%/build
+cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%UserProfile%/zarlok -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER="C:/MinGW/bin/gcc" -DCMAKE_CXX_COMPILER="C:/MinGW/bin/g++.exe" ..
+gmake
+gmake install
+set PATH=%UserProfile%/zarlok;%PATH%
+echo %PATH%
+
---
+++ build_linux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export zarlok_src=`pwd`
+mkdir ${zarlok_src}/build -p
+cd ${zarlok_src}/build
+cmake .. -DCMAKE_INSTALL_PREFIX=${HOME}/usr -DCMAKE_BUILD_TYPE=Release
+gmake
+gmake install
+export PATH=${HOME}/usr/bin:${PATH}
