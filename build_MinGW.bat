d91a2a1a3fa3b28c14ca4ebc54696a3784296a85
Rafal Lalik <RafalLalik@ph.tum.de>
2012-05-01 15:31:23 +0200

    build_MinGW fixed

--- build_MinGW.bat
+++ build_MinGW.bat
@@ -1,9 +1,9 @@
 mkdir build
 cd build
 
-cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX=%UserProfile%/zarlok -DCMAKE_BUILD_TYPE=Release ..
-mimgw32-make
+cmake -G "MinGW Makefiles" -DCMAKE_INSTALL_PREFIX="%UserProfile%/zarlok" -DCMAKE_BUILD_TYPE=Release ..
+mingw32-make
 mingw32-make install
 
-set PATH=%UserProfile%/zarlok;%PATH%
+set PATH="%UserProfile%/zarlok;%PATH%"
 echo %PATH%
