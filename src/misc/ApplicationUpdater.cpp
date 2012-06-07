e0ff67394a869f6ca436fcece877eedb34ba219e
Rafal Lalik <RafalLalik@ph.tum.de>
2012-06-07 22:31:08 +0200

    Dist build implemented for LINUX (DEB, RPM, TBZ2) and for WIN32 (ISS), bugfix #4

--- src/misc/ApplicationUpdater.cpp
+++ src/misc/ApplicationUpdater.cpp
@@ -25,6 +25,7 @@
 #include <QFile>
 #include <qprocess.h>
 #include <QMessageBox>
+#include <QStringBuilder>
 
 ApplicationUpdater::ApplicationUpdater(QObject * parent) : QObject(parent), replyUpdateCheckRequest(NULL), replyDownloadRequest(NULL) {
 	nam = new QNetworkAccessManager(this);
