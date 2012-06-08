5f43a695eb8e9320d092066ce703f5a3dc71de35
Rafal Lalik <RafalLalik@ph.tum.de>
2012-06-08 20:37:29 +0200

    Separated versionizator for UNIX and WIN32

--- VersionFileBuilder.cpp
+++ VersionFileBuilder.cpp
@@ -4,7 +4,13 @@
 #include "version.h"
 
 int main() {
+#if defined(_WIN32)
+	std::ofstream ofs("build_version_number_win32.txt");
+#elif defined(__unix__)
+	std::ofstream ofs("build_version_number_unix.txt");
+#else
 	std::ofstream ofs("build_version_number.txt");
+#endif
 
 	ofs << ZARLOK_VERSION;
 
