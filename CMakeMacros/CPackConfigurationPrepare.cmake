b74323883116e253ffc45dc3b689d205fb708f5d
Rafal Lalik <RafalLalik@ph.tum.de>
2012-06-10 16:41:34 +0200

    Rewritten packaging sysytem, experimental NSIS support

---
+++ CMakeMacros/CPackConfigurationPrepare.cmake
@@ -0,0 +1,13 @@
+find_program (vfb_exe vfb)
+MESSAGE (STATUS "VFB: " ${vfb_exe})
+
+execute_process (COMMAND ${vfb_exe} OUTPUT_VARIABLE RELEASE_VERSION ERROR_VARIABLE RELEASE_VERSION_ERROR OUTPUT_STRIP_TRAILING_WHITESPACE)
+MESSAGE (STATUS ${RELEASE_VERSION})
+
+if ( UNIX AND NOT APPLE )
+configure_file(${ZARLOK_SOURCE_DIR}/CMakeMacros/CPackConfigurationLinux.cmake.in CPackConfigurationLinux.cmake @ONLY)
+endif ( UNIX AND NOT APPLE )
+
+if ( MINGW )
+configure_file(${ZARLOK_SOURCE_DIR}/CMakeMacros/CPackConfigurationWindows.cmake.in CPackConfigurationWindows.cmake @ONLY)
+endif ( MINGW )
